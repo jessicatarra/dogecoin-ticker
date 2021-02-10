65eb36d8a5b90538c6c82b6443c2328e476ed2c4
Jessica Tarra <jessictarra@gmail.com>
2021-02-10 14:26:07 -0300

    update logo background add pricecard component and fetch api using axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import PriceCard from './components/PriceCard';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
+  const [ticker, setTicker] = useState({
+    low: 0,
+    high: 0,
+    last: 0,
+  });
+
+  useEffect(() => {
+    async function getDogecoinPrice() {
+      const {data} = await axios.get('https://nitinr-cors.herokuapp.com/https://api.wazirx.com/api/v2/tickers/dogeusdt');
+      setTicker(data.ticker);
+    }
+    getDogecoinPrice();
+    setInterval(() => getDogecoinPrice(), 10000);
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} width={150} height={150} alt="Dogecoin Logo" />
+      <h1 className="title">Live Dogecoin Price</h1>
+      <h5 className="subtitle">Dogecoin To The Moon 🚀🌕</h5>
+      <div className="prices-container">
+        <PriceCard type="low" price={ticker.low} />
+        <PriceCard type="high" price={ticker.high} />
+        <PriceCard type="current" price={ticker.last} />
+      </div>
+      <p>
+        Dogecoin price updated every 10 seconds from {' '}
+	<a href="https://wazirx.com/">WazirX api</a>
+      </p>
     </div>
   );
-}
+};
 
 export default App;
